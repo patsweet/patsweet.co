87d3e54eeebf573222b2d7fadd948c3889856070
Patrick Sweet <patmsweet@gmail.com>
2014-03-04 11:56:47 -0500

    Update Fabfile to collectstatic

--- fabfile.py
+++ fabfile.py
@@ -7,4 +7,5 @@ def deploy():
     code_dir = '/home/patsweet/webapps/patsweet/patsweet'
     with cd(code_dir):
         run("git pull")
+        run("python2.7 manage.py collectstatic --noinput")
         run("../apache2/bin/restart")
