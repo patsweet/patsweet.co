05775a721df948c8933b58e54c68a75cef2b69d8
Patrick Sweet <patmsweet@gmail.com>
2014-03-04 19:58:06 -0500

    more fabfile tweaking

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,4 @@ def deploy():
     code_dir = '/home/patsweet/webapps/patsweet/patsweet'
     with cd(code_dir):
         run("git pull")
-        run("pip-2.7 install -r requirements.txt")
-        run("python2.7 manage.py collectstatic --noinput")
         run("../apache2/bin/restart")
