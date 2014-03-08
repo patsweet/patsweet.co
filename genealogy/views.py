db1b299397e6791f14ed951c008db7898806e457
Patrick Sweet <patmsweet@gmail.com>
2014-03-07 23:16:31 -0500

    Protect family api behind auth

--- genealogy/views.py
+++ genealogy/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import FamilyMember
 from .serializers import FamilyMemberSerializer
+from rest_framework import permissions
 
 
 class FamilyMemberViewSet(viewsets.ReadOnlyModelViewSet):
@@ -9,3 +10,4 @@ class FamilyMemberViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = FamilyMember.objects.all().order_by('birthday')
     serializer_class = FamilyMemberSerializer
+    permission_classes = [permissions.IsAuthenticated]
