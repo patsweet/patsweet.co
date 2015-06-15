e9ec0a0ca45321c700c3d223a224f46d196f2a8c
Patrick Sweet <patmsweet@gmail.com>
2015-06-15 22:15:18 +0000

    fix caching issue

--- genealogy/urls.py
+++ genealogy/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url, include
+from django.contrib.auth.decorators import login_required
 from .views import FamilyMemberViewSet, IndexView, FamilyList, FamilyTreeList, FamilyDetail
 from rest_framework.routers import DefaultRouter
 
@@ -8,7 +9,7 @@ router.register(r'members', FamilyMemberViewSet)
 urlpatterns = patterns('',
     # API
     url(r'^api/', include(router.urls)),
-    url(r'^$', FamilyList.as_view(), name='family-index'),
-    url(r'^tree/$', FamilyTreeList.as_view(), name='family-tree'),
-    url(r'^(?P<pk>\d+)/$', FamilyDetail.as_view(), name="family-detail"),
+    url(r'^$', login_required(FamilyList.as_view()), name='family-index'),
+    url(r'^tree/$', login_required(FamilyTreeList.as_view()), name='family-tree'),
+    url(r'^(?P<pk>\d+)/$', login_required(FamilyDetail.as_view()), name="family-detail"),
 )
--- patsweet/settings.py
+++ patsweet/settings.py
@@ -88,6 +88,7 @@ INSTALLED_APPS = (
 
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.gzip.GZipMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -95,6 +96,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 )
 
 ROOT_URLCONF = 'patsweet.urls'
